2e874deba4a476ec0d7e9f41d66e1762cc470d18
Uri-X <philbertkuria@gmail.com>
2026-02-20 23:26:04 +0300

    added a script to load zones

--- backend/routes/zones.py
+++ backend/routes/zones.py
@@ -1,10 +1,9 @@
 from flask import Blueprint, jsonify
-from backend.db.connection import get_db_connection
-
+from db.connection import get_db_connection
 
 zones_bp = Blueprint('zones', __name__)
 
-@zones_bp.route('/', methods=['GET'])
+@zones_bp.route('/zones', methods=['GET'])
 def get_zones():
     conn = get_db_connection()
     zones = conn.execute("SELECT * FROM dim_zones").fetchall()
